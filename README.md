5f217cd188acf3b8541c735277b90f2aa70dbf5d
biobot <biobot94@gmail.com>
2022-01-20 21:30:03 +0800

    init
    Signed-off-by: biobot <biobot94@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# LGMDGPU
+
+Try to port [LGMD](https://github.com/Biobots/LGMD) from Python to C++ with GPU acceleration.
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <kompute/Kompute.hpp>
+#include <nlohmann/json.hpp>
+#include <opencv2/opencv.hpp>
+int main()
+{
+	return 0;
+}
